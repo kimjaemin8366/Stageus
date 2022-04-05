59916b56d9cb87220c455b04819267ed3cbb2389
kimjaemin8366 <hse07003@naver.com>
2022-04-05 09:36:14 +0900

    4.4

--- Youtube/nav_menu.js
+++ Youtube/nav_menu.js
@@ -1,13 +1,61 @@
-
 function full_menu(){
-    var nav = document.getElementById("nav_full");
-    var body = document.body;
-    if(nav.style.display == "block"){
-        nav.style.display = "none";
-        body.style.overflow="scroll";
+    var nav_visible = document.getElementById("nav_full");
+    var nav_animation_css = document.getElementById("nav_full_list");
+    var var_background_css = document.getElementById("nav_full_background");
+    var main_css = document.getElementById("main_content");
+    var body_css = document.body;
+    var now_window_size = window.outerWidth;
+
+    function close_menu(){
+        body_css.style.overflowY ="scroll";
+        nav_visible.style.visibility= "hidden";
+        nav_animation_css.style.left = "-300px";
+        main_css.style.marginLeft = "";
     }
-    else{
-        nav.style.display = "block";
-        body.style.overflow="hidden";
+
+    function open_menu(){
+        nav_visible.style.visibility = "visible";
+        nav_animation_css.style.left ="0px";
     }
+
+    
+    if(now_window_size<1500){ /* 브라우저 크기가 작을 경우*/
+        var_background_css.style.display = "block";
+        nav_animation_css.style.transition = ".25s";
+
+        if(nav_visible.style.visibility == "visible"){ /* 메뉴 닫을 때*/
+            close_menu();
+        }
+        else{ /* 메뉴 열 때*/
+            body_css.style.overflow="hidden";
+            open_menu();
+        }
+    }
+
+    else{ /* 브라우저 크기가 클 경우 */
+        var_background_css.style.display= "none";
+        nav_animation_css.style.transition = "0s";
+
+        if(nav_visible.style.visibility == "visible") { /* 메뉴를 닫을 때*/
+            close_menu();
+        }
+
+        else{ /* 메뉴를 열 때*/
+            main_css.style.marginLeft = "256px";
+            open_menu();
+        }
+    }
+}
+
+function nav_size_check(){
+    var nav_size = window.innerHeight-76;
+    document.getElementById("nav_full_content").style.height = nav_size+"px";
+    
+}
+
+window.onload = nav_size_check();
+
+window.onresize = function(){
+    var timer = null;
+    timer = setTimeout(nav_size_check(), 250);
 }
--- baseball.js
+++ baseball.js
@@ -1,11 +1,104 @@
 
-var first_num = 0
-var second_num = 0
-var thrid_num = 0
-var selected_room = 0
+var selected_number = [,,];
+var selected_room = 0;
+
+var random_number_array = [,,];
+
+function make_random_number(){
+    for(i=0; i<=2; i++){
+        var random_picked = Math.floor(Math.random()*10);
+
+        if(random_number_array.indexOf(random_picked) == -1){
+            random_number_array[i] = random_picked;
+        }else{
+            i--;
+        }
+        
+    }
+    console.log(random_number_array);
+}
+
+function number_room_design(){
+    for(i=0; i<=2; i++){
+        var room = document.getElementById("number_room_"+i).style;
+        if(i!=selected_room){
+            room.backgroundColor = "white"; 
+        }else{
+            room.backgroundColor = "lightgrey";  
+        }
+    }
+}
+
+function new_game(){
+    make_random_number();
+    for(i=0; i<=2; i++){
+        document.getElementById("number"+i).innerHTML = "";
+        selected_number[i] = null;
+    }
+    document.getElementById("now_ball_count").innerHTML = "";
+    selected_room = 0;
+    number_room_design();
+}
+
+new_game()
 
 function put(num){
-    x = num;
-    
+    if(num == selected_number[0]|| num == selected_number[1] || num == selected_number[2]){
+        alert("이미 고른 숫자입니다.")
+    }else{
+        selected_number[selected_room] = parseInt(num);
+        document.getElementById("number"+selected_room).innerHTML = num;
+    }
 }
+
+function room_clicked(room_num){
+    if(room_num == "number_room_0"){
+        selected_room = 0;
+    }else if(room_num == "number_room_1"){
+        selected_room = 1;
+    }else if(room_num == "number_room_2"){
+        selected_room = 2;
+    }else{ 
+        alert("에러")
+    }
+    number_room_design()
+}
+
+function ball_count(){
+    var strike = 0;
+    var ball = 0;
+    for(i=0; i<=2; i++){
+        if(random_number_array[i] == selected_number[i]){
+            strike += 1;
+        }else if(random_number_array.indexOf(selected_number[i]) != -1){
+            ball += 1;
+        }
+    }
+
+    if(strike==3){
+        document.getElementById("now_ball_count").innerHTML = "3 Strike! 새 게임을 눌러주세요";
+    }else{
+        document.getElementById("now_ball_count").innerHTML = "strike: " + strike + " ball: " + ball;
+    }
+}
+
+function erase(){
+    document.getElementById("number"+selected_room).innerHTML = "";
+    selected_number[selected_room] = null;
+}
+
+// function put(num){
+//     var picked_num = num;
+    
+//     if(selected_room==1){
+//         put_num_to_room(picked_num)
+//     }
+//     else if(selected_room==2){
+
+//     }
+//     else{
+        
+
+//     }
+// }
 
